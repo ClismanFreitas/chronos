87452fd635ec922ee3f76d537eb68e820c3fed5f
Clisman Freitas <clismanfreitas.com@gmail.com>
2025-08-21 17:52:33 -0300

    nextcycle

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,10 +5,13 @@ import { DeafaltInput } from "../Input";
 import { useRef } from "react";
 import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
 
 export function MainForm() {
-  const { setState } = useTaskContext()
+  const { state, setState } = useTaskContext()
   const taskNameInput = useRef<HTMLInputElement>(null)
+
+  const nextCycle = getNextCycle(state.currentCycle)
 
   const handleCreateNewTask = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -38,7 +41,7 @@ export function MainForm() {
         ...prevState,
         config: { ...prevState.config },
         activeTask: newTask,
-        currentCycle: 1, // Conferir
+        currentCycle: nextCycle,
         secondsRemaining, // Conferir
         formattedSecondsRemaining: '00:00', // Conferir
         tasks: [...prevState.tasks, newTask],
