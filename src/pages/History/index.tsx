6d74a76added472cfa49bdea5186aa3b704cc6c0
Clisman Freitas <clismanfreitas.com@gmail.com>
2026-03-21 00:20:54 -0300

    feat: dados e converçoes da tabela feito

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -4,8 +4,13 @@ import { DefaultButton } from "../../components/DefaultButton"
 import { Heading } from "../../components/Heading"
 import { MainTemplate } from "../../templates/MainTemplate"
 import styles from "./styles.module.css"
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
+import { formatDate } from "../../utils/formatDate"
+import { getTaskStatus } from "../../utils/getTaskStatus"
 
 export const History = () => {
+
+    const { state } = useTaskContext()
 
     return (
         <MainTemplate>
@@ -36,15 +41,25 @@ export const History = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {Array.from({length:20}).map(()=> {return (
-                                <tr>
-                                <td>Estudar</td>
-                                <td>25min</td>
-                                <td>20/04/2025 08:00</td>
-                                <td>Completa</td>
-                                <td>Foco</td>
-                            </tr>
-                            )})} 
+                            {state.tasks.map((task)=> {
+                            const taskTypeDictionary = {
+                                workTime: "Foco",
+                                shortBreakTime: "Descanso curto",
+                                longBreakTime: "Descanso longo"
+                            }
+
+                            return (
+                                <tr key={task.id}>
+                                    <td>{task.name}</td>
+                                    <td>{task.duration}min</td>
+                                    <td>
+                                        {formatDate(task.startDate)}
+                                    </td>
+                                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                                    <td>{taskTypeDictionary[task.type]}</td>
+                                </tr>
+                            )
+                            })} 
                         </tbody>
                     </table>
                 </div>
